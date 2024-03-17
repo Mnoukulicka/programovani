cb424c3196f9e6458aeb3029754f7f3fb0e68d0b
Josef Barták <bartakjosef@zstrnka.cz>
2024-03-17 20:43:25 +0100

    Turret done, needs tryout

--- engine3/src/classes/Turret.js
+++ engine3/src/classes/Turret.js
@@ -26,13 +26,14 @@ class Turret extends Component{
         if (this.gameObject.pos.y > PLayer.pos.y) {this.y1 = this.gameObject.pos.y, this.y2 = PLayer.pos.y} else {this.y2 = this.gameObject.pos.y, this.y1 = PLayer.pos.y}
     this.object = 1
     this.objects[2]
+    if (Math.sqrt((this.y2 - this.y1)*(this.y2 - this.y1) + (this.x2 - this.x1)*(this.x2 - this.x1))< 300){
     for (this.object of this.objects){
         this.s = this.objects[this.object]
         this.srb = this.s.getComponent(RectRenderer)
-        this.srbx1 = s.pos.x - srb.size.x / 2
-        this.srbx2 = srb.size.x
-        this.srby1 = s.pos.y - srb.size.y / 2
-        this.srby2 = srb.size.y
+        this.srbx1 = this.s.pos.x - srb.size.x / 2
+        this.srbx2 = this.srb.size.x
+        this.srby1 = this.s.pos.y - srb.size.y / 2
+        this.srby2 = this.srb.size.y
         if (this.srbx2 < this.x1) this.objects.splice(IndexOf(this.object))
         else if (this.srby2 > this.y1) this.objects.splice(IndexOf(this.object))
         else if (this.srbx1 > this.x2) this.objects.splice(IndexOf(this.object))
@@ -40,6 +41,12 @@ class Turret extends Component{
     this.object ++
     rect(s.pos.x - srb.size.x / 2, s.pos.y - srb.size.y / 2, srb.size.x, srb.size.y)
     }
+    this.player = PLayer.getComponent(RectRenderer)
+    this.playerx1 = PLayer.pos.x - this.player.size.x / 2
+    this.playerx2 = this.player.size.x
+    this.playery1 = PLayer.pos.y - this.player.size.y / 2
+    this.playery2 = this.player.size.y
+
     if (this.objects.length > 0){
         for (let i = 3; i <= (this.x2 - this.x1)/3; i+3) {
             this.linexes.push(i)
@@ -62,10 +69,10 @@ class Turret extends Component{
     for (this.object of this.objects){
         this.s = this.objects[this.objects]
         this.srb = this.s.getComponent(RectRenderer)
-        this.srbx1 = s.pos.x - srb.size.x / 2
-        this.srbx2 = srb.size.x
-        this.srby1 = s.pos.y - srb.size.y / 2
-        this.srby2 = srb.size.y
+        this.srbx1 = this.s.pos.x - this.srb.size.x / 2
+        this.srbx2 = this.srb.size.x
+        this.srby1 = this.s.pos.y - this.srb.size.y / 2
+        this.srby2 = this.srb.size.y
         this.counter = 0
         this.cycle = 0
         for  (let i = 0; i <= this.longerlinestuff; i + 1){
@@ -75,7 +82,6 @@ class Turret extends Component{
             this.counter = 0
             this.liney = this.lineys[cycle]
             this.cycle ++
-            this.moves ++
         
         }
     }
@@ -84,51 +90,27 @@ class Turret extends Component{
             this.linex = this.linexes[cycle]
             this.cycle ++
         this.liney = this.lineys[i]
-        this.moves ++
     }}
         if (this.linex < this.srbx2 && this.linex > this.srbx1 && this.liney > this.srby2 && this.liney < this.srby1) this.objects.splice(IndexOf(this.object));{
              this.detection = 0
         i = this.longerlinestuff + 1
 
         }
+        if (this.linex < this.playerx2 && this.linex > this.playerx1 && this.liney > this.playery2 && this.liney < this.playery1){
+            this.detection = 1 
+            i = this.longerlinestuff + 1
+        }
          this.object ++
          this.moves ++
-         if (this.moves > 90)  i = this.longerlinestuff + 1
     }
+    this.breakpoints.slice(0, this.breakpoints.length)
+    this.linexes.slice(0, this.linexes.length)
+    this.lineys.slice(0, this.lineys.length)
 }
 }
-
+}
    
 }
-class Scout extends Component{
-    constructor (mypos, myspeed, turret){
-     this.myspeed = myspeed
-     this.rb = this.getcomponent(DynamicBoxCollider)
-     this.mypos = mypos
-     this.turret = turret
-     this.target = createVector(PLayer.x, PLayer.y)
-     this.mydir = Vector.sub (target, this.mypos)
-     this.mydir.add(random(-0.2, 0.2))
-     this.mydir.setMag(this.myspeed)
-     this.rb.vel.x = this.mydir.x
-     this.rb.vel.y = this.mydir.y
-    
-    }
- 
- 
-    draw(){
-      if (this.mypos.x > width) this.destroy()
-      if (this.mypos.x < 0) this.destroy()
-      if (this.mypos.y > height) this.destroy()
-      if (this.mypos.y < 0) this.destroy()
-      this.rb.onCollisionEnter = (col) => {
-        if (!col.collider.getComponent(Player)) {
-            turrets2(IndexOf(this.turret)) = 1
-        }
-        this.destroy()
-    }
-    }
- }
 class Bullet extends Component{
     constructor (mypos, myspeed){
      this.myspeed = myspeed
