0cadf1c791ee71c1e4a427b133aedaf25ce88689
Mnoukulicka <bartakjosef@zstrnka.cz>
2024-04-25 17:11:05 +0200

    Wordcount

--- WordCounter/WordCounter/Program.cs
+++ WordCounter/WordCounter/Program.cs
@@ -9,17 +9,35 @@
             WordCounter counter = new WordCounter();
             Console.WriteLine("Input text:");
             string? text = Console.ReadLine();
-            if (text == null) {
+            if (text == null)
+            {
                 Console.WriteLine("Error: No text given");
                 return;
             }
+
             int count = counter.CountWords(text);
             Console.WriteLine($"The text has {count} words.");
         }
 
         public int CountWords(string text)
         {
-            return 0;
+            int wordCount = 0;
+            Console.WriteLine(text.Length);
+            for (int i = 1; i < text.Length; i++)
+            {
+                char charac = text[i];
+                char charca = text[i - 1];
+                if (i == 1 && !(char.IsWhiteSpace(charca) || charca is '.' or ','))
+                {
+                    wordCount++;
+                }
+                else if (!(char.IsWhiteSpace(charac) || charac is '.' or ',') && (char.IsWhiteSpace(charca) || charca is '.' or ','))
+                {
+                    wordCount++;
+                }
+            }
+            return wordCount;
+           
         }
     }
 }
