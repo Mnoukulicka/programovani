cad11c3717e3513ff4f7314966859b55aa231abb
Mnoukulicka <bartakjosef@zstrnka.cz>
2024-05-02 17:05:24 +0200

    B

--- C#/SecondLargest/SecondLargest/Program.cs
+++ C#/SecondLargest/SecondLargest/Program.cs
@@ -8,12 +8,11 @@
 
              //SecondLargest(list; x);
         }
-        public static int FindLargest(int[] list)
+        public static int FindLargest(IEnumerable<int> list)
         {
-            int x = list[0];
-            for (int i = 1; i < list.Length; i++)
+            int x = 0;
+            foreach (var y in list)
             {
-                int y = list[i];
                 if (y > x)
                 {
                     x = y;
@@ -54,7 +53,15 @@
 
         public static int NthLargest(int[] list, int n)
         {
-            return FindLargest(list);
+            int[] list2 = new int[n];
+            // int[] list3 = new int[list.Length];
+            var list3 = new List<int>(list);
+            for (int i = 0; i < n; i++)
+            {
+                list2[i] = FindLargest(list3);
+                list3.Remove(FindLargest(list3));
+            }
+             return list2[n];
         }
     }
 }
